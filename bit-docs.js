2373be18eb0ed5200b1a3dbacd12f8f69793cbdc
Justin Meyer <justinbmeyer@gmail.com>
2019-04-22 20:28:20 -0500

    add to html

--- bit-docs.js
+++ bit-docs.js
@@ -1,1 +1,10 @@
-module.exports = function(){};
+module.exports = function(bitDocs){
+	var pkg = require("./package.json");
+
+	var dependencies = {};
+	dependencies[pkg.name] = pkg.version;
+
+	bitDocs.register("html", {
+		dependencies: dependencies
+	});
+};
